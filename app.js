13a86807e5b027183a3edd39f2768ec968900cd5
Cameron Graham <camy117@gmail.com>
2019-02-24 11:09:44 +0100

    basic backend using external api

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const  t9 = require('t9-rest-api');
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.use(cors());
+app.use('/api/words', t9);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
